e62554d1eaa95352566ccd704ad731746fc6b4a8
VietAn98 <44429962+VietAn98@users.noreply.github.com>
2019-11-30 17:18:48 +0700

    Chỉnh sửa giao diện

--- ui-user/src/components/registerComponent.js
+++ ui-user/src/components/registerComponent.js
@@ -12,38 +12,24 @@ class Register extends React.PureComponent {
           <div className="w3-agile_mail_grids justify-center">
             <div className="col-md-7 w3-agile_mail_grid_right">
               <Form action="#" method="post">
-                <div className="col-md-6 col-sm-6 contact_left_grid">
+                <div className="col-md-9 col-sm-9 contact_left_grid">
                   <input
                     type="text"
                     name="name"
                     placeholder="Họ và tên"
                     required=""
                   />
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required=""
-                  />
                 </div>
-                <div className="col-md-6 col-sm-6 contact_left_grid">
-                  <div>
-                    <input
-                      type="text"
-                      name="phonenumber"
-                      placeholder="Số điện thoại"
-                      required=""
-                    />
-                  </div>
+                <div className="col-md-3 col-sm-3 contact_left_grid">
                   <div className="form-checkbox float-left">
                     <Form.Check
-                      className="pt-4 ml-3 checkbox"
+                      className="pt-2 checkbox"
                       type="checkbox"
                       id="male"
                       label="Nam"
                     />
                     <Form.Check
-                      className="pt-4 mt-3 ml-5 checkbox"
+                      className="pt-2 mt-3 ml-5 checkbox"
                       type="checkbox"
                       id="female"
                       label="Nữ"
@@ -51,7 +37,15 @@ class Register extends React.PureComponent {
                   </div>
                 </div>
                 <br />
-                <div className="col-md-12 col-sm-12 pt-4 pb-4">
+                <div className="col-md-12 col-sm-12">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required=""
+                  />
+                </div>
+                <div className="col-md-12 col-sm-12 pt-4">
                   <input
                     type="text"
                     name="address"
@@ -76,9 +70,9 @@ class Register extends React.PureComponent {
                   />
                 </div>
                 <div className="col-md-12 col-sm-12 pb-4">
-                  <h4 className="float-left">
-                    Bạn muốn đăng kí tài khoản để trở thành:
-                  </h4>
+                  <h5 className="float-left text-white pt-4">
+                    <b>Bạn muốn đăng kí tài khoản để trở thành:</b>
+                  </h5>
                   <Form.Control as="select" className="select-form">
                     <option className="black-title">Học viên</option>
                     <option className="black-title">Giáo viên</option>
@@ -86,8 +80,8 @@ class Register extends React.PureComponent {
                 </div>
                 <input type="submit" value="Đăng ký" />
               </Form>
-              <h4 className="mt-4">Hoặc</h4>
-              <h5>Đăng kí tài khoản bằng</h5>
+              <h4 className="mt-4 text-white">Hoặc</h4>
+              <h5 className="text-white">Đăng kí tài khoản bằng</h5>
               <a href="#" className="fb-btn">
                 <i className="fa fa-facebook fa-fw"></i> Facebook
               </a>
