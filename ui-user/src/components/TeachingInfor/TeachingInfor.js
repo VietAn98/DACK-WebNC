baca22d4c7f409c5d5422801bea5cb3ffe6a6d67
VietAn98 <44429962+VietAn98@users.noreply.github.com>
2019-12-01 15:43:24 +0700

    Giao diện Đăng ký dạy học

--- ui-user/src/components/TeachingInfor/TeachingInfor.js
+++ ui-user/src/components/TeachingInfor/TeachingInfor.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Container, Form } from 'react-bootstrap';
+import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import "./TeachingInfor.css";
 
 class TeachingInfor extends React.PureComponent {
   onChangeSelection = (e) => {
-    console.log(e.target.value);
+    const arr = [];
+    arr.push(e.target.value);
+    console.log(e.target.length);
   };
 
   render() {
@@ -12,9 +15,9 @@ class TeachingInfor extends React.PureComponent {
         <Container>
           <div
             style={{
-              backgroundColor: 'rgba(0,0,0,0.5)',
-              borderRadius: '20px',
-              paddingBottom: '3rem',
+              backgroundColor: "rgba(0,0,0,0.5)",
+              borderRadius: "20px",
+              paddingBottom: "3rem"
             }}
           >
             <h3 className="w3layouts-heading white-title">
@@ -23,7 +26,7 @@ class TeachingInfor extends React.PureComponent {
             <div className="w3-agile_mail_grids justify-center">
               <div className="col-md-7 w3-agile_mail_grid_right">
                 <Form action="#" method="post">
-                  <div className="col-md-9 col-sm-9 contact_left_grid">
+                  <div className="col-md-12 col-sm-12 contact_left_grid">
                     <input
                       type="text"
                       name="name"
@@ -31,105 +34,85 @@ class TeachingInfor extends React.PureComponent {
                       required=""
                     />
                   </div>
-                  <div className="col-md-3 col-sm-3 contact_left_grid">
-                    <div className="form-checkbox float-left">
-                      <Form.Check
-                        className="checkbox"
-                        type="checkbox"
-                        id="male"
-                        label="Nam"
-                      />
-                      <Form.Check
-                        className="mt-3 ml-5 checkbox"
-                        type="checkbox"
-                        id="female"
-                        label="Nữ"
-                      />
-                    </div>
-                  </div>
                   <br />
-                  <div className="col-md-12 col-sm-12">
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      required=""
-                    />
-                  </div>
-                  {/* <div className="col-md-12 col-sm-12 pt-4"></div> */}
-                  <div className="col-md-6 col-sm-6 contact_left_grid pt-3">
-                    <input
-                      type="text"
-                      name="address"
-                      placeholder="Địa chỉ"
-                      required=""
-                    />
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="Nhập mật khẩu"
-                      required=""
-                    />
-                  </div>
-                  <div className="col-md-3 col-sm-3 contact_left_grid pt-3">
-                    <Form.Control as="select" className="select-form">
-                      <option className="black-title">Thành phố</option>
-                      <option className="black-title">...</option>
-                    </Form.Control>
-                  </div>
-                  <div className="col-md-3 col-sm-3 contact_left_grid pt-3">
-                    <Form.Control as="select" className="select-form">
-                      <option className="black-title">Quận</option>
-                      <option className="black-title">...</option>
-                    </Form.Control>
-                  </div>
-                  <div className="col-md-6 col-sm-6 contact_left_grid pt-1">
-                    <input
-                      type="password"
-                      name="re-password"
-                      placeholder="Nhập lại mật khẩu"
-                      required=""
-                    />
-                  </div>
                   <div className="col-md-12 col-sm-12 pb-4">
                     <h5 className="float-left text-white pt-4">
-                      <b>Bạn muốn đăng kí tài khoản để trở thành:</b>
+                      <b>Chuyên ngành của bạn:</b>
                     </h5>
                     <Form.Control
                       as="select"
                       className="select-form"
                       onChange={this.onChangeSelection}
                     >
-                      <option className="black-title" value="hocvien">
-                        Học viên
+                      <option className="black-title" value="software">
+                        Công nghệ phần mềm
                       </option>
-                      <option className="black-title" value="giaovien">
-                        Giáo viên
+                      <option className="black-title" value="system">
+                        Hệ thống thông tin
+                      </option>
+                      <option className="black-title" value="network">
+                        Mạng máy tính
                       </option>
                     </Form.Control>
                     <div className="clearfix" />
                   </div>
-                  <input
-                    type="submit"
-                    value="Đăng ký"
-                    className="signUpSubmit"
-                  />
+                  <div className="col-md-5 col-sm-5 pb-4">
+                    <h5 className="float-left text-white pt-4">
+                      <b>Hãy chọn các kỹ năng của bạn:</b>
+                    </h5>
+                    <Form.Control
+                      as="select"
+                      multiple
+                      className="select-form"
+                      onChange={this.onChangeSelection}
+                    >
+                      <option value="software">Công nghệ phần mềm</option>
+                      <option value="system">Hệ thống thông tin</option>
+                      <option value="network">Mạng máy tính</option>
+                    </Form.Control>
+                    <div className="clearfix" />
+                  </div>
+                  <div className="col-md-1 col-sm-1 pt-10 mr-3">
+                    <Button className="mb-2">{'>'}</Button>
+                    <Button>{'<'}</Button>
+                  </div>
+                  <div className="col-md-5 col-sm-5 pb-4">
+                    <h5 className="float-left text-white pt-4">
+                      <b>Kỹ năng được chọn:</b>
+                    </h5>
+                    <Form.Control
+                      as="select"
+                      multiple
+                      className="select-form"
+                      onChange={this.onChangeSelection}
+                    >
+                      {/* <option className="black-title" value="software">
+                        Công nghệ phần mềm
+                      </option>
+                      <option className="black-title" value="system">
+                        Hệ thống thông tin
+                      </option>
+                      <option className="black-title" value="network">
+                        Mạng máy tính
+                      </option> */}
+                    </Form.Control>
+                  </div>
+                  <div className="col-md-12 col-sm-12 contact_left_grid">
+                    <textarea
+                      type="textarea"
+                      name="intro"
+                      placeholder="Giới thiệu đôi chút về bản thân bạn nào"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="submit"
+                      value="Hoàn thành"
+                      className="signUpSubmit"
+                    />
+                  </div>
                 </Form>
-
-                <div style={{ textAlign: "center" }}>
-                  <h4 className="mt-4 text-white">Hoặc</h4>
-                  <h5 className="text-white">Đăng kí tài khoản bằng</h5>
-                  <a href="#" className="fb-btn">
-                    <i className="fa fa-facebook fa-fw" />
-                    {' '}
-                    Facebook
-                  </a>
-                  <a href="#" className="google-btn">
-                    <i className="fa fa-google fa-fw" />
-                    {' '}
-                    Google+
-                  </a>
-                </div>
               </div>
             </div>
           </div>
