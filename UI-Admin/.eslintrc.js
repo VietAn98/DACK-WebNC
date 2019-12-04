590e5896f17fcfa2af3b4dfcddc0979f3183aaf4
VietAn98 <anh.vy98@gmail.com>
2019-12-04 08:55:41 +0700

    update UI-Admin và ESLint

--- UI-Admin/.eslintrc.js
+++ UI-Admin/.eslintrc.js
@@ -1,27 +1,43 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+	env: {
+		browser: true,
+		es6: true,
+		node: true
+	},
+	extends: [ 'plugin:react/recommended', 'airbnb' ],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly'
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	plugins: [ 'react' ],
+	rules: {
+		'linebreak-style': 0,
+		'react/jsx-filename-extension': [
+			1,
+			{
+				extensions: [ '.js', '.jsx' ]
+			}
+		],
+		'react/prop-types': 0,
+		'no-underscore-dangle': 0,
+		'import/imports-first': [ 'error', 'absolute-first' ],
+		'import/newline-after-import': 'error',
+		'import/prefer-default-export': 0,
+		semi: 'error',
+		singleQuote: 0,
+		'comma-dangle': 'off',
+		indent: [ 'error', 'tab' ],
+		'no-tabs': 0,
+		'react/jsx-indent': 'off',
+		'react/jsx-indent-props': 'off',
+		'import/no-named-as-default': 0
+	},
+	parser: 'babel-eslint'
 };
--- UI-Admin/src/App.js
+++ UI-Admin/src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import AdminSignIn from "./containers/SignInContainer";
-import CreateAccountContainer from "./containers/CreateAccountContainer";
-import PageNotFound from "./components/PageNotFound"
-import "./App.css";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import AdminSignIn from './containers/SignInContainer';
+import CreateAccountContainer from './containers/CreateAccountContainer';
+import PageNotFound from './components/PageNotFound';
+import './App.css';
 
 export class App extends React.PureComponent {
-  render() {
-    return (
-      <main>
-        <Switch>
-          <Route path="/admin/login" exact component={AdminSignIn} />
-          <Route path="/admin/create-admin" exact component={CreateAccountContainer} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </main>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Switch>
+					<Route path="/admin/login" exact component={AdminSignIn} />
+					<Route path="/admin/create-admin" exact component={CreateAccountContainer} />
+					<Route component={PageNotFound} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
