3a2837f9f74ccfd40d3e68b5559a7280ed26eb01
lehung22297 <44271319+lehung22297@users.noreply.github.com>
2019-12-04 11:19:11 +0700

    update API-admin, cho phép truy cập https

--- API-Admin/app.js
+++ API-Admin/app.js
@@ -5,9 +5,34 @@ const passport = require("passport");
 require('./middleware/passport');
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+
+  // allow options method work, ask experts for more
+  if (req.method === "OPTIONS") {
+    return res.status(200).json({});
+  }
+  next();
+};
+
+app.use(allowCrossDomain);
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "*");
+    next(); 
+  });
 
 require("./routes")(app);
 
